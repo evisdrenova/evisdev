a2c85a34d462200736cb183a2da535c5771a6d79
Evis Drenova <evis@usenucleus.cloud>
2025-08-03 21:47:24 -0700

    feat(Header.tsx): update flex layout and social icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,10 +16,23 @@ export default function Header() {
           <AvatarFallback>Ed</AvatarFallback>
         </Avatar>
         <div className="flex-1">
-          <div className="flex flex-row items-center gap-2 justify-between">
+          <div className="flex flex-row items-center gap-4">
             <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-300 mb-2">
               Evis Drenova
             </h1>
+            <div className="flex gap-4">
+              <FaGithub className="w-5 h-5 text-gray-900 dark:text-gray-200 cursor-pointer transition-colors" />
+              <div className="w-5 h-5 flex items-center justify-center cursor-pointer group">
+                <svg
+                  viewBox="0 0 24 24"
+                  className="w-4 h-4  text-gray-900 dark:text-gray-200 transition-colors fill-current"
+                  aria-label="X (formerly Twitter)"
+                >
+                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                </svg>
+              </div>
+              <FaLinkedin className="w-5 h-5 text-gray-900 dark:text-gray-200  cursor-pointer transition-colors" />
+            </div>
             {/* <ThemeToggle /> */}
           </div>
           <p className="text-gray-600 text-sm dark:text-gray-400 mb-5 w-[70%]">
@@ -41,19 +54,6 @@ export default function Header() {
               and brain computer interfaces.
             </span>
           </p>
-          <div className="flex gap-4">
-            <FaGithub className="w-5 h-5 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200 cursor-pointer transition-colors" />
-            <div className="w-5 h-5 flex items-center justify-center cursor-pointer group">
-              <svg
-                viewBox="0 0 24 24"
-                className="w-4 h-4 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200 k transition-colors fill-current"
-                aria-label="X (formerly Twitter)"
-              >
-                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-              </svg>
-            </div>
-            <FaLinkedin className="w-5 h-5 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200  cursor-pointer transition-colors" />
-          </div>
         </div>
       </div>
     </header>
