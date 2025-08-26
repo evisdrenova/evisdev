08aae1537594e58d7ac401977a008e284a531edc
Evis Drenova <evis@usenucleus.cloud>
2025-08-25 19:31:30 -0700

    feat: add PostHog analytics and rewrites for ingest paths

--- next.config.js
+++ next.config.js
@@ -5,6 +5,21 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 const withMDX = createMDX({
